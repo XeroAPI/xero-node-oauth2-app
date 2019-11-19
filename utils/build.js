7e3fc542b4e1af7aa77cb03597b66f532ef84cc7
serknight <chrisknight.mail@gmail.com>
2019-11-19 13:22:48 -0700

    production asset compilation file pathing

--- utils/build.js
+++ utils/build.js
@@ -6,10 +6,10 @@ try {
   // Remove current build
   fs.removeSync('./dist/');
   // Copy front-end files
-  fs.copySync('./src/public', './dist/public');
+  fs.copySync('./public', './dist/public');
   fs.copySync('./src/views', './dist/views');
   // Transpile the typescript files
-  childProcess.exec('tsc --build tsconfig.prod.json');
+  childProcess.exec('tsc --build tsconfig.json');
 } catch (err) {
   console.log(err);
 }
